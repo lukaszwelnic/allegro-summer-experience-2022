6a931a98723522fa4546ddd434885926c79a9f37
Łukasz Wełnic <44496335+lukaszwelnic@users.noreply.github.com>
2022-04-24 21:07:44 +0200

    Final update

--- api.py
+++ api.py
@@ -12,16 +12,16 @@ def fetching_failed_exception(status_code: int):
         detail=f'Code executed with status {status_code}. Fetching failed.')
 
 
-def fetch_user_data(username, token):
+def fetch_user_data(user_login, token):
 
-    if token is None or token == "":
+    if token is None or token == '':
         headers = {'Authorization': ''}
     else:
         headers = {'Authorization': 'token ' + token}
 
-    url_user = 'https://api.github.com/users/' + username
+    url_user = 'https://api.github.com/users/' + user_login
 
-    url_repos = 'https://api.github.com/users/' + username + '/repos'
+    url_repos = 'https://api.github.com/users/' + user_login + '/repos'
 
     login = None
     name = None
@@ -48,8 +48,6 @@ def fetch_user_data(username, token):
             repo_languages = requests.get(url=repo['languages_url'], headers=headers)
             if repo_languages.status_code == 200:
                 repo_languages = repo_languages.json()
-            elif repo_languages.status_code == 403:
-                raise HTTPException(status_code=403, detail='Forbidden Access')
             else:
                 raise fetching_failed_exception(repo_languages.status_code)
             repository = Repository(repo['name'], repo_languages)
@@ -61,40 +59,40 @@ def fetch_user_data(username, token):
         raise fetching_failed_exception(response_repos.status_code)
 
 
-@app.get("/user_info", status_code=200)
-async def get_user_info(user: str = "", token: str | None = None):
-    user_data = fetch_user_data(user, token)
+@app.get('/user_info', status_code=200)
+async def get_user_info(login: str = '', token: str | None = None):
+    user_data = fetch_user_data(login, token)
     user_info = {
-        "login": user_data.user_login,
-        "name": user_data.user_name,
-        "bio": user_data.user_bio,
-        "languages_used": []
+        'login': user_data.user_login,
+        'name': user_data.user_name,
+        'bio': user_data.user_bio,
+        'languages_used': []
     }
     languages_info = user_data.get_all_language_stats()
     for lang_name, size_bytes in languages_info.items():
-        user_info["languages_used"].append(
-            {"language_name": lang_name,
-             "size_bytes": size_bytes})
+        user_info['languages_used'].append(
+            {'language_name': lang_name,
+             'size_bytes': size_bytes})
     return user_info
 
 
-@app.get("/repositories_info", status_code=200)
-async def get_repositories_info(user: str = "", token: str | None = None):
-    user_data = fetch_user_data(user, token)
+@app.get('/repositories_info', status_code=200)
+async def get_repositories_info(login: str = '', token: str | None = None):
+    user_data = fetch_user_data(login, token)
     repositories = {
-        "login": user,
-        "repositories": []
+        'login': login,
+        'repositories': []
     }
     for repos in user_data.user_repositories:
         repo = {
-            "repository_name": repos.repository_name,
-            "languages": []
+            'repository_name': repos.repository_name,
+            'languages': []
         }
         for lang_name, size_bytes in repos.repository_languages.items():
             lang_info = {
-                "language_name": lang_name,
-                "size_bytes": size_bytes
+                'language_name': lang_name,
+                'size_bytes': size_bytes
             }
-            repo["languages"].append(lang_info)
-        repositories["repositories"].append(repo)
+            repo['languages'].append(lang_info)
+        repositories['repositories'].append(repo)
     return repositories
